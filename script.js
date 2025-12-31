76769f533220e01935b3ebde610f2667f8bcbe25
Viccler <42334457+Viccorn@users.noreply.github.com>
2025-12-31 19:34:10 +0200

    Make the grid boxes dynamic in size and add button for a new grid

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 
-let temp;
+
 let container = document.querySelector(".container");
 
+let btnAdd = document.querySelector(".btnAdd");
 
-for (let i = 1; i <= 16; i++) {
-    temp = document.createElement("div");
-    temp.className = "gridBox";
-    container.appendChild(temp);
+
+function createGrid() {
+    let boxesPerRow;
+    while (true) {
+        boxesPerRow = prompt("How many boxes per side you want? (max 100) ", 4);
+        if (boxesPerRow <= 100 && boxesPerRow > 0) {
+            break;
+        }
+    }
+
+    for (let i = 1; i <= (boxesPerRow * boxesPerRow); i++) {
+        let temp = document.createElement("div");
+        temp.style.backgroundColor = "white";
+        temp.style.width = `calc(${100 / boxesPerRow}%)`;
+        temp.style.aspectRatio = "1 / 1";
+        container.appendChild(temp);
+        temp.addEventListener('mouseover', function (e) {
+            temp.style.backgroundColor = getRandomColor();
+
+            setTimeout(() => {
+                temp.style.backgroundColor = "white";
+            }, 35000);
+            // body
+        });
+
+    }
 
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+
+btnAdd.addEventListener('click', function (e) {
+    container.textContent = '';
+    createGrid();
+});
+
+
